23952de4b7cbb3d411875e5f1341f147fd3c8b69
zzaku <enzo.makked@gmail.com>
2023-05-31 10:46:59 +0200

    research route added'

--- serveur/api/routes/catalogue.js
+++ serveur/api/routes/catalogue.js
@@ -14,6 +14,29 @@ router.get('/allmasterclass', async (req, res) => {
     }
 });
 
+
+//GET BACK ALL MASTERCLASS BY WORK'S NAME
+router.get('/research/work', async (req, res) => {
+    const workName = req.query.name
+    try{
+        myDatabase.query(`SELECT mc.id AS masterclass_id, mc.nom AS masterclass_nom, mc.langue AS masterclass_langue, mc.status AS masterclass_status,o.id AS oeuvre_id, o.nom_compositeur AS oeuvre_nom_compositeur, o.nom AS oeuvre_nom,p.id AS professeur_id, p.nom AS professeur_nom,
+        u.id AS utilisateur_id, u.nom AS utilisateur_nom,
+        i.nom AS instrument_nom
+        FROM Masterclass mc
+        JOIN oeuvre o ON mc.id_oeuvre = o.id
+        JOIN professeurs p ON o.id_professeurs = p.id
+        JOIN utilisateur u ON mc.id_utilisateur = u.id
+        JOIN instruments i ON i.id_oeuvre = o.id
+        WHERE o.nom LIKE '%${workName}%'
+        ORDER BY masterclass_id ASC`, (error, results, fields) => {
+            if (error) throw error;
+            res.status(200).json(results)
+          });
+    }catch(err){
+        res.json({message: err})
+    }
+});
+
 //GET BACK ALL MASTERCLASS BY INSTRUMENT
 router.get('/category', async (req, res) => {
     try{
