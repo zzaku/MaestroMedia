9a4b1ea07fdadad95c4b4f41c4b041f45df44be7
mehdihnc <mehdiharchenif@gmail.com>
2023-06-01 16:29:41 +0200

    crud, design, font

--- client/inc/functions/back/process_login.php
+++ client/inc/functions/back/process_login.php
@@ -6,17 +6,19 @@ function signIn() {
     $email = $_POST['email'];
     $password = $_POST['password'];
     
-    $stmt = $pdo->prepare("SELECT password FROM utilisateur WHERE email = ?");
+    $stmt = $pdo->prepare("SELECT * FROM utilisateur WHERE email = ?");
     $stmt->execute([$email]);
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
     
     if ($row && password_verify($password, $row['password'])) {
         $_SESSION['loggedin'] = true;
+        $_SESSION['user'] = $row; // Stock all user information in the session
+        unset($_SESSION['user']['password']); // For security reasons, do not store password in session
         header('Location: ./index.php?page=home');
         exit;
     } else {
         header('Location: ../../pages/login/login.php?error=1');
-
         exit;
     }
 }
+
--- client/index.php
+++ client/index.php
@@ -10,8 +10,13 @@ session_start();
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Saline</title>
+
+        <link rel="stylesheet" href="./index.css">
         <link rel="stylesheet" href="./pages/home/home.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-KldBTSdRdEhZ1cIeicBASL0vuk+rjDz5ixSfFVypxnDRjD/8yU0l4aTPC/RPvRVtuvID3YjtS8WoyOJw7hEZGw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet">
         <script src="https://kit.fontawesome.com/72d345e137.js" crossorigin="anonymous"></script>
+        
     </head>
     <body>
         <?php 
