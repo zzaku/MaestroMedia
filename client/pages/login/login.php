ad184f04c9df175dbafb1bc627b3fe30638c179c
zzaku <enzo.makked@gmail.com>
2023-06-02 10:19:42 +0200

    placeholder login added

--- client/pages/login/login.php
+++ client/pages/login/login.php
@@ -6,10 +6,10 @@ if(isset($_POST['submit'])) {
 ?>
  <form action="" method="post" class="login-form">
   <label for="email" class="form-label">Adresse e-mail :</label>
-  <input type="email" id="email" name="email" class="form-input">
+  <input type="email" id="email" name="email" class="form-input" placeholder="Adresse mail">
 
   <label for="password" class="form-label">Mot de passe :</label>
-  <input type="password" id="password" name="password" class="form-input">
+  <input type="password" id="password" name="password" class="form-input" placeholder="Mot de passe">
 
   <input type="submit" name="submit" value="Connexion" class="form-button">
 </form>
