0fa60575111772b9e5b773f6986a139e222126fd
mehdihnc <mehdiharchenif@gmail.com>
2023-06-02 06:36:47 +0200

    style login

--- client/pages/login/login.php
+++ client/pages/login/login.php
@@ -4,12 +4,13 @@ if(isset($_POST['submit'])) {
     signIn();
 }
 ?>
-  <form action="" method="post">
-    <label for="email">Adresse e-mail :</label>
-    <input type="email" id="email" name="email">
+ <form action="" method="post" class="login-form">
+  <label for="email" class="form-label">Adresse e-mail :</label>
+  <input type="email" id="email" name="email" class="form-input">
 
-    <label for="password">Mot de passe :</label>
-    <input type="password" id="password" name="password">
+  <label for="password" class="form-label">Mot de passe :</label>
+  <input type="password" id="password" name="password" class="form-input">
 
-    <input type="submit" name="submit" value="Connexion">
-  </form>
+  <input type="submit" name="submit" value="Connexion" class="form-button">
+</form>
+
